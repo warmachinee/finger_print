0c57a1c2775e987252438321b1909c99f5051f03
P.R.E.M <31282752+warmachinee@users.noreply.github.com>
2018-11-20 01:33:15 +0700

    finger_print_v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const btn = {
+  fontSize: '1.5rem',
+  padding: '1rem 2rem',
+  margin: '1rem',
+  cursor: 'pointer'
+};
+const btnStopHover = {
+  fontSize: '1.5rem',
+  padding: '1rem 2rem',
+  margin: '1rem',
+  cursor: 'pointer',
+  background: '#ccc'
+};
+const btnLogout = {
+  fontSize: '1.5rem',
+  padding: '1rem 2rem',
+  margin: '1rem',
+  border: 'none',
+  background: 'red',
+  color: 'white'
+};
+const titleDiv = {
+  margin: '0 1rem',
+  display: 'flex',
+}
+const titleP = {
+  fontSize: '1.5rem',
+}
+const titleValue = {
+  fontSize: '1.5rem',
+  fontWeight: 'bold',
+  padding: '0 1.5rem'
+}
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      scanState: false,
+      btnStopHover: btn,
+      btnLogoutHover: btnLogout,
+    }
+  }
+  scanFinger = () =>{
+    this.setState((state)=>{return {scanState: !state.scanState}})
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button style={btn} onClick={this.scanFinger}>Scan</button>
+        {(this.state.scanState)?(
+          <div>
+            <div style={titleDiv}>
+              <p style={titleP}>Name : </p>
+              <p style={titleValue}>User1</p>
+            </div>
+            <div style={titleDiv}>
+              <p style={titleP}>Time : </p>
+              <p style={titleValue}>06:00</p>
+            </div>
+            <div style={{display: 'flex'}}>
+              <button
+                style={this.state.btnLogoutHover}
+                onMouseEnter={()=>this.setState({btnLogoutHover: btn})}
+                onMouseLeave={()=>this.setState({btnLogoutHover: btnLogout})}
+                onClick={this.scanFinger}>Logout</button>
+              <button
+                style={this.state.btnStopHover}
+                onMouseEnter={()=>this.setState({btnStopHover: btnStopHover})}
+                onMouseLeave={()=>this.setState({btnStopHover: btn})}
+                onClick={this.scanFinger}>Stop</button>
+            </div>
+          </div>
+        ):(null)}
       </div>
     );
   }
